83f9aec5275bda4fb454d08969d5b1dc6047a351
espriter <esprit@espriter.net>
2022-07-02 13:15:21 +0900

    degree change

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ servo_pin = 18
 SERVO_MAX_DUTY = 12   # 서보의 최대(180도) 위치의 주기
 SERVO_MIN_DUTY = 3    # 서보의 최소(0도) 위치의 주기
 now = datetime.datetime.now()
+today = now.day
 
 # 서보모터 init
 GPIO.setmode(GPIO.BCM)
@@ -15,12 +16,11 @@ pwm = GPIO.PWM(servo_pin, 50)  # 50Hz (서보모터 PWM 동작을 위한 주파�
 pwm.start(0)  # 서보의 0도 위치(0.6ms)이동:값 3.0은 pwm주기인 20ms의 3%를 의미하므로,0.6ms됨.
 
 # 각도 설정 및 주파수 전환
-degree = 91.7
+# degree = 91.7
+degree = 180
 duty = SERVO_MIN_DUTY+(degree*(SERVO_MAX_DUTY-SERVO_MIN_DUTY)/180.0)
 print("Degree: {} to {}(Duty)".format(degree, duty))
 
-# 날짜 확인
-today = now.day
 
 # 홀수 짝수에 따라 회전 횟수 변경
 if (today % 2) == 0:
