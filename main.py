559cc7504d4b50344e959e1fc262d61a282af730
espriter <esprit@espriter.net>
2022-07-02 11:02:51 +0900

    flag up/down test 3

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin, GPIO.OUT)
 pwm = GPIO.PWM(servo_pin, 50)  # 50Hz (서보모터 PWM 동작을 위한 주파수)
 pwm.start(3.0)  # 서보의 0도 위치(0.6ms)이동:값 3.0은 pwm주기인 20ms의 3%를 의미하므로,0.6ms됨.
-time.sleep(15)
+
 
 # Flag Up
-pwm.ChangeDutyCycle(7.5)
-time.sleep(10)
+# pwm.ChangeDutyCycle(7.5)
+# time.sleep(10)
 
 # Flag Down
-pwm.ChangeDutyCycle(7.5)
-time.sleep(10)
+# pwm.ChangeDutyCycle(7.5)
+# time.sleep(10)
 
 # for cnt in range(0, 3):  # 0부터 3미만까지(3번) for문의 내용을 반복
 #     pwm.ChangeDutyCycle(3.0)  # 서보모터를 0도로 회전(이동)
