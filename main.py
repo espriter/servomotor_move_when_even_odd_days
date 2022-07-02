d67a3ab567531981e7a8d3a77970e43af9b89e97
espriter <esprit@espriter.net>
2022-07-02 13:19:10 +0900

    make def()

--- main.py
+++ main.py
@@ -9,30 +9,32 @@ SERVO_MIN_DUTY = 3    # 서보의 최소(0도) 위치의 주기
 now = datetime.datetime.now()
 today = now.day
 
-# 서보모터 init
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo_pin, GPIO.OUT)
-pwm = GPIO.PWM(servo_pin, 50)  # 50Hz (서보모터 PWM 동작을 위한 주파수)
-pwm.start(0)  # 서보의 0도 위치(0.6ms)이동:값 3.0은 pwm주기인 20ms의 3%를 의미하므로,0.6ms됨.
+def run_motor():
+   # 서보모터 init
+   GPIO.setmode(GPIO.BCM)
+   GPIO.setup(servo_pin, GPIO.OUT)
+   pwm = GPIO.PWM(servo_pin, 50)  # 50Hz (서보모터 PWM 동작을 위한 주파수)
+   pwm.start(0)  # 서보의 0도 위치(0.6ms)이동:값 3.0은 pwm주기인 20ms의 3%를 의미하므로,0.6ms됨.
 
-# 각도 설정 및 주파수 전환
-degree = 91.7
-# degree = 180
-duty = SERVO_MIN_DUTY+(degree*(SERVO_MAX_DUTY-SERVO_MIN_DUTY)/180.0)
-print("Degree: {} to {}(Duty)".format(degree, duty))
+   # 각도 설정 및 주파수 전환
+   degree = 91.7
+   duty = SERVO_MIN_DUTY+(degree*(SERVO_MAX_DUTY-SERVO_MIN_DUTY)/180.0)
+   print("Degree: {} to {}(Duty)".format(degree, duty))
 
+   # 91.7도 변환
+   pwm.ChangeDutyCycle(duty)
+   pwm.stop()
+   GPIO.cleanup()
+   sleep(5)
 
 # 홀수 짝수에 따라 회전 횟수 변경
 if (today % 2) == 0:
    print("{0} is Even".format(today))
-   pwm.ChangeDutyCycle(duty)
-   sleep(1)
+   run_motor()
    print("짝수 Flag Up")
 
 else:
    print("{0} is Odd".format(today))
-   pwm.ChangeDutyCycle(duty) # flag up
+   run_motor()
    sleep(1)
 
-pwm.stop()
-GPIO.cleanup()
